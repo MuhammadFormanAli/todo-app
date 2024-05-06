0e10a9eef3e156f6400e647a1b061e96262dd19b
MuhammadFormanAli <muhammadformaanali@gmail.com>
2024-05-07 00:42:03 +0600

    Meet Our Team Added

--- app/components/home/MeetOurTeam.jsx
+++ app/components/home/MeetOurTeam.jsx
@@ -1,10 +1,56 @@
 import React from "react";
+import TeamMemberCard from "./TeamMemberCard";
+import TeamMember1 from "@/public/OurTeam/member-1.png";
+import TeamMember2 from "@/public/OurTeam/member-2.png";
+import TeamMember3 from "@/public/OurTeam/member-3.png";
+import TeamMember4 from "@/public/OurTeam/member-4.png";
+import TeamMember5 from "@/public/OurTeam/member-5.png";
+import TeamMember6 from "@/public/OurTeam/member-6.png";
 
 const MeetOurTeam = () => {
+  const teamMembers = [
+    {
+      id: 1,
+      name: "Jasim Uddin",
+      designation: "CEO & Founder",
+      image: TeamMember1,
+    },
+    {
+      id: 2,
+      name: "Pronob Ghosh",
+      designation: "Chairman",
+      image: TeamMember2,
+    },
+    {
+      id: 3,
+      name: "K M Jubayer Abdullah",
+      designation: "Marketing Manager",
+      image: TeamMember3,
+    },
+    {
+      id: 4,
+      name: "Zarin Jasia Oishi",
+      designation: "Social Media Manager",
+      image: TeamMember4,
+    },
+    {
+      id: 5,
+      name: "H.M. Nazmus Sakib",
+      designation: "HR & Admin",
+      image: TeamMember5,
+    },
+    {
+      id: 6,
+      name: "Nafisa T. Disha",
+      designation: "UI UX Design Team Lead",
+      image: TeamMember6,
+    },
+  ];
   return (
-    <div className="relative py-10">
-      <div className="h-full w-full absolute top-0 left-0 -z-50">
-        
+    <div className="relative py-10 flex flex-col gap-20 ">
+      <div className="h-full w-full absolute top-0 left-0 -z-50 flex items-center justify-evenly overflow-hidden">
+        <div className="h-60 w-60 bg-[#CD1CE6] bg-opacity-60 blur-[210px]"></div>
+        <div className="h-60 w-60  bg-[#2194FF] bg-opacity-60 blur-[250px]"></div>
       </div>
 
       {/* Team members heading  */}
@@ -22,7 +68,11 @@ const MeetOurTeam = () => {
       </div>
 
       {/* place for showing member cards  */}
-      <div></div>
+      <div className=" max-w-[1300px] mx-auto px-4 grid grid-cols-3 gap-10 place-content-center">
+        {teamMembers.map((member) => (
+          <TeamMemberCard key={member.id} member={member} />
+        ))}
+      </div>
     </div>
   );
 };
--- app/components/home/TeamMemberCard.jsx
+++ app/components/home/TeamMemberCard.jsx
@@ -1,9 +1,37 @@
+import Image from 'next/image';
 import React from 'react';
+import BackgroundImage from '@/public/OurTeam/team-member-bg-3.png'
+import Medium from '@/public/OurTeam/medium.png'
+import Facebook from '@/public/OurTeam/facebook.png'
+import Twitter from '@/public/OurTeam/twitter.png'
+import LinkedIn from '@/public/OurTeam/linkedIn.png'
+import Be from '@/public/OurTeam/be.png'
+import Ball from '@/public/OurTeam/ball.png'
+import Link from 'next/link';
 
-const TeamMemberCard = () => {
+
+const TeamMemberCard = ({member}) => {
+    const {id,name,designation,image} = member
     return (
-        <div>
-            
+        <div className='border rounded-lg max-w-[360px] p-4 flex flex-col gap-4 bg-white'>
+            <div className='w-full relative flex items-center justify-center overflow-hidden'>
+                <Image className='absolute top-0 p-3 left-[50%] z-0 translate-x-[-50%]' src={BackgroundImage} alt='Team Member Image' />
+                <Image className='z-20' src={image} alt='Team Member Image' />
+            </div>
+           <div className='text-[#0D050F]  font-medium'>
+           <h1 className='text-3xl '>{name}</h1>
+            <p className='text-lg'>{designation}</p>
+           </div>
+            <hr/>
+            <div className='flex justify-between items-center'>
+
+            <Link className='h-7 w-7 bg-black rounded-full p-1 bg-opacity-15 flex items-center justify-center ' href='#'><Image src={Medium} alt='icon'/></Link>
+            <Link className='h-7 w-7 bg-black rounded-full p-1 bg-opacity-15 flex items-center justify-center ' href='#'><Image src={Facebook} alt='icon'/></Link>
+            <Link className='h-7 w-7 bg-black rounded-full p-1 bg-opacity-15 flex items-center justify-center ' href='#'><Image src={Twitter} alt='icon'/></Link>
+            <Link className='h-7 w-7 bg-black rounded-full p-1 bg-opacity-15 flex items-center justify-center ' href='#'><Image src={LinkedIn} alt='icon'/></Link>
+            <Link className='h-7 w-7 bg-black rounded-full p-1 bg-opacity-15 flex items-center justify-center ' href='#'><Image src={Be} alt='icon'/></Link>
+            <Link className='h-7 w-7 bg-black rounded-full p-1 bg-opacity-15 flex items-center justify-center ' href='#'><Image src={Ball} alt='icon'/></Link>
+            </div>
         </div>
     );
 };
